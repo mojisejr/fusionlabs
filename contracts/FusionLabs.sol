07d2f5afc2c399b1a0e2960c2af80dc73f94b22c
nonthasak laoluerat <nonthasak.l@Appels-iMac.local>
2022-11-03 07:20:20 +0700

    updates fee, payment setbase uri

--- contracts/FusionLabs.sol
+++ contracts/FusionLabs.sol
@@ -35,19 +35,27 @@ contract FusionLabs is Ownable, IERC721Receiver {
     uint256 totalFusioned;
     uint256 maxLocked;
 
+    uint256 fee;
+    address payable devAddr;
+
     event Received(uint256 _tokenId, address _owner, bytes data);
     event Fusioned(uint256 _hostTokenId, uint256 _stimulusTokenId);
+    event CancelFusion(uint256 _hostTokenId, uint256 _stimulusTokenId);
+    event SetFee(uint256 _newFee);
+    event Withdrawn(uint256 _amounts);
 
     constructor(  
         IERC721 _host,
         IERC721 _stimulus,
         IResult _result,
-        uint256 _maxLocked
+        uint256 _maxLocked,
+        address payable _devAddr
     ) {
         host = _host;
         stimulus = _stimulus;
         result = _result;
         maxLocked = _maxLocked;
+        devAddr = _devAddr;
     }
 
     //Modifiers
@@ -88,10 +96,17 @@ contract FusionLabs is Ownable, IERC721Receiver {
         _;
     }
 
-    //Locking 
+    modifier Available() {
+        require(totalFusioned < maxLocked, 'reach max supply');
+        _;
+    }
+
+    //LOCKING FUNCITON
+    //////////////////** */ 
     function lock(uint256 _hostTokenId, uint256 _stimulusTokenId) public 
         onlyHostOwner(_hostTokenId)
         onlyStimulusOwner(_stimulusTokenId)
+        Available()
     { 
         pairs[_hostTokenId] = _stimulusTokenId;
         locked[msg.sender].push(_hostTokenId);
@@ -99,54 +114,29 @@ contract FusionLabs is Ownable, IERC721Receiver {
         _lockStimulus(_stimulusTokenId);
     }
 
-    function getFusionablePairs(address _owner) public view returns(string memory) {
-        string memory useables;
-        uint256[] memory lockedHosts = locked[_owner];
-        for(uint256 i; i < lockedHosts.length; ++i) {
-            if(hosts[lockedHosts[i]].locked && !hosts[lockedHosts[i]].used) {
-                useables = string(abi.encodePacked(useables,'{"host":',toString(lockedHosts[i]),', "stimulus":',toString(pairs[lockedHosts[i]]),'},'));
-            }
-        }
-        useables = substring(useables, 0, bytes(useables).length - 1);
-        useables = string(abi.encodePacked('[',useables,']'));
-    
-        return useables;
-    }
-
-
-    function _lockHost (uint256 _tokenId) internal {
-        host.safeTransferFrom(msg.sender, address(this), _tokenId, "0x01"); 
-        hosts[_tokenId].owner = msg.sender;
-        hosts[_tokenId].locked = true; 
-    }
-
-    function _lockStimulus (uint256 _tokenId) internal {
-        stimulus.safeTransferFrom(msg.sender, address(this), _tokenId, "0x02");
-        stimuli[_tokenId].owner = msg.sender;
-        stimuli[_tokenId].locked = true;
-    }
-
-    function fusion(uint256 _hostTokenId, uint256 _stimulusTokenId) public 
+    //FUSION FUNCTION
+    /////////////////** */
+    function fusion(uint256 _hostTokenId, uint256 _stimulusTokenId) public payable
         onlyLockedHostOwner(_hostTokenId)
         onlyLockedStimulusOwner(_stimulusTokenId)
         onlyFusionAble(_hostTokenId, _stimulusTokenId)
+        Available()
     {
+        require(msg.value == fee, 'invalid fee amount');
         hosts[_hostTokenId].used = true;
         stimuli[_stimulusTokenId].used = true;
         fusioned[_hostTokenId] = true; 
+        _increaseTotalFusioned();
         _mintTo(msg.sender);
         emit Fusioned(_hostTokenId, _stimulusTokenId);
     }
 
-    function getFusionedOf(uint256 _hostTokenId) public view returns(uint256) {
-        return pairs[_hostTokenId];
-    }
-
-    function _mintTo(address _to) internal {
-        result.mint(_to);
-    }
-
-    function cancelFusion(uint256 _hostTokenId) public onlyLockedHostOwner(_hostTokenId) cancelAble(_hostTokenId) {
+    //CANCEL FUNCTION
+    /////////////////** */
+    function cancelFusion(uint256 _hostTokenId) public 
+        onlyLockedHostOwner(_hostTokenId) 
+        cancelAble(_hostTokenId) 
+    {
         uint256 stimulusTokenId = pairs[_hostTokenId];
         _withdrawHost(_hostTokenId);
         _withdrawStimulus(stimulusTokenId);
@@ -156,22 +146,32 @@ contract FusionLabs is Ownable, IERC721Receiver {
                 _removeCancelLocked(i, lockedHost);
             }
         }
+        emit CancelFusion(_hostTokenId, stimulusTokenId);
     }
 
-    function _removeCancelLocked(uint256 index, uint256[] storage array) internal {
-        for(uint i = index; i < array.length-1; i++){
-            array[i] = array[i+1];      
-        }
-        array.pop();
+    //SETTER FUNCTIONS
+    //////////////////
+
+    function setFusionFee(uint256 _newFee) external {
+        require(_newFee > 0, 'invalid fee amounts');
+        fee = _newFee;
+
+        emit SetFee(_newFee);
     }
 
-    function _withdrawHost(uint256 _hostTokenId) internal {
-        host.safeTransferFrom(address(this), msg.sender, _hostTokenId , "0x01");
+    function withdrawFee() external {
+        (bool success, bytes memory returndata) = devAddr.call{value: address(this).balance}("");
+        require(success, 'withdraw failed.');
+        emit Withdrawn(address(this).balance);
     }
-    
-    function _withdrawStimulus(uint256 _stimulusTokenId) internal {
-        stimulus.safeTransferFrom(address(this), msg.sender, _stimulusTokenId , "0x02");
+
+    function setDev(address payable _newDev) public  onlyOwner {
+        devAddr = _newDev;
     }
+
+
+    //VIEWS FUNCTIONS
+    /////////////////
 
     function isHostLocked(uint256 _tokenId) public view returns(bool) {
         return hosts[_tokenId].locked;
@@ -182,11 +182,11 @@ contract FusionLabs is Ownable, IERC721Receiver {
     }
     
     function isFusionable(uint256 _hostTokenId, uint256 _stimulusTokenId) public view returns(bool) {
-        require(isHostLocked(_hostTokenId), 'not locked yet');
-        require(isStimulusLocked(_stimulusTokenId), 'not locked yet');
-        require(!hosts[_hostTokenId].used, 'host has been fusioned');
-        require(!stimuli[_stimulusTokenId].used, 'stimuli has been fusioned');
-        require(!fusioned[_hostTokenId], 'token has been fusion');
+        require(isHostLocked(_hostTokenId), 'not locked yet.');
+        require(isStimulusLocked(_stimulusTokenId), 'not locked yet.');
+        require(!hosts[_hostTokenId].used, 'host has been fusioned.');
+        require(!stimuli[_stimulusTokenId].used, 'stimuli has been fusioned.');
+        require(!fusioned[_hostTokenId], 'token has been fusioned.');
         return pairs[_hostTokenId] == _stimulusTokenId;
     }
 
@@ -196,10 +196,38 @@ contract FusionLabs is Ownable, IERC721Receiver {
         return true;
     }
 
+    function getFusionablePairs(address _owner) public view returns(string memory) {
+        string memory useables;
+        uint256[] memory lockedHosts = locked[_owner];
+        for(uint256 i; i < lockedHosts.length;) {
+            if(hosts[lockedHosts[i]].locked && !hosts[lockedHosts[i]].used) {
+                useables = string(abi.encodePacked(useables,'{"host":',toString(lockedHosts[i]),', "stimulus":',toString(pairs[lockedHosts[i]]),'},'));
+            }
+            unchecked {
+                ++i;
+            }
+        }
+        useables = substring(useables, 0, bytes(useables).length - 1);
+        useables = string(abi.encodePacked('[',useables,']'));
+    
+        return useables;
+    }
+
+    function getPairOf(uint256 _hostTokenId) public view returns(uint256) {
+        return pairs[_hostTokenId];
+    }
+
+    function getBalance() public view returns(uint256) {
+        return address(this).balance;
+    }
+
     function onERC721Received(address operator, address from,  uint256 _tokenId, bytes calldata data) external override returns(bytes4) {
         emit Received(_tokenId, from, data);
         return this.onERC721Received.selector;
     }
+
+    //HELPER FUNCTIONS
+    //////////////////
 
     function toString(uint256 value) internal pure returns (string memory) {
         if (value == 0) {
@@ -231,5 +259,46 @@ contract FusionLabs is Ownable, IERC721Receiver {
             result[i - startIndex] = strBytes[i];
         }
         return string(result);
+    }
+
+    //INTERNAL FUNCTIONS
+    ////////////////////
+
+    function _lockHost (uint256 _tokenId) internal {
+        host.safeTransferFrom(msg.sender, address(this), _tokenId, "0x00"); 
+        hosts[_tokenId].owner = msg.sender;
+        hosts[_tokenId].locked = true; 
+    }
+
+    function _lockStimulus (uint256 _tokenId) internal {
+        stimulus.safeTransferFrom(msg.sender, address(this), _tokenId, "0x00");
+        stimuli[_tokenId].owner = msg.sender;
+        stimuli[_tokenId].locked = true;
+    }
+
+    function _withdrawHost(uint256 _hostTokenId) internal {
+        host.safeTransferFrom(address(this), msg.sender, _hostTokenId , "0x00");
+    }
+    
+    function _withdrawStimulus(uint256 _stimulusTokenId) internal {
+        stimulus.safeTransferFrom(address(this), msg.sender, _stimulusTokenId , "0x00");
+    }
+
+    function _removeCancelLocked(uint256 index, uint256[] storage array) internal {
+        for(uint i = index; i < array.length-1;){
+            array[i] = array[i+1];      
+            unchecked {
+                ++i;
+            }
+        }
+        array.pop();
+    }
+
+    function _mintTo(address _to) internal {
+        result.mint(_to);
+    }
+
+    function _increaseTotalFusioned() internal {
+        ++totalFusioned;
     }
 }
--- contracts/Mutant.sol
+++ contracts/Mutant.sol
@@ -7,9 +7,20 @@ import "./ERC721A/extensions/ERC721AQueryable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Mutant is ERC721A, ERC721AQueryable, Ownable {  
-  address labs;
 
-  event Minted(address _to, uint256 tokenId);
+  address labs;
+  uint256 max_supply;
+
+  mapping(uint256 => string) baseURI;
+  mapping(uint256 => bool) completed;
+  uint256 requestCounter = 0;
+  
+
+
+  event Minted(address _to, uint256 _tokenId);
+  event SetBaseURI(uint256 _tokenId, string _baseURI);
+  event RequestBaseURI(uint256 _tokenId);
+
   constructor(address _labs) ERC721A("MUTANT", "MUT") {
     labs = _labs;
   }
@@ -19,5 +30,24 @@ contract Mutant is ERC721A, ERC721AQueryable, Ownable {
     uint256 currentId = _nextTokenId() - 1; 
     _mint(_to, 1);
     emit Minted(_to, currentId);
+  }
+
+  function setBaseURI(uint256 _tokenId, string memory _baseURI) public onlyOwner {
+    require(!completed[_tokenId], 'uri has been set');
+    completed[_tokenId] = true;
+    baseURI[_tokenId] = _baseURI;
+    emit SetBaseURI(_tokenId, _baseURI);
+  }
+
+  function tokenURI(uint256 _tokenId) public view override(ERC721A, IERC721A) returns(string memory) {
+    require(completed[_tokenId], 'token uri not completed');
+    return baseURI[_tokenId];      
+  }
+
+  function requestBaseURI(uint256 _tokenId) public {
+    require(ownerOf(_tokenId) == msg.sender, 'only token owner');
+    require(!completed[_tokenId], 'uri has already been set');
+    ++requestCounter;
+    emit RequestBaseURI(_tokenId);
   }
 }
